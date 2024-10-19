93a5e9522eca0e7f4cbf8b1851d5efc43ac1c4c2
José Murilo <82318481+jmurilojm@users.noreply.github.com>
2024-10-19 12:38:00 -0300

    Add files via upload

---
+++ catalogo.js
@@ -0,0 +1,53 @@
+const listaDeFilmes = [
+  {
+    id: '0001',
+    nome: 'A Caverna Azul',
+    link: 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/ODAyY2ZkNDctMmM5OS00YzkyLTk0MjQtNDNlMWRkMGEwYzZk.m3u8',
+    imagem: 'https://cdn.syncdev.com.br//uploads/video_thumb/12938.jpg',
+    ano: 2024,
+    usuarios: [10001]
+  },
+  {
+    id: '0002',
+    nome: 'Lá Fora',
+    link: 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/OGQwMjdiYjYtMTgyZi00ZGM4LWIzZDItZjYyYWRmNzc2Mjk1.m3u8',
+    imagem: 'https://cdn.syncdev.com.br//uploads/video_thumb/12926.jpg',
+    ano: 2024,
+    usuarios: [10001]
+  },
+  {
+    id: '0003',
+    nome: 'Melhor Viúva Do Que Mal Acompanhada',
+    link: 'https://space.vscine.biz/filmes/melhorviuva/playlist.m3u8',
+    imagem: 'https://cdn.syncdev.com.br//uploads/video_thumb/12587.jpg',
+    ano: 2024,
+    usuarios: [10001]
+  },
+  {
+    id: '0004',
+    nome: 'Terrifier 3 / Aterrorizante 3 (HDCAM)',
+    link: 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/NmYyNGI3ZTYtMjA3My00NTIzLTkyZmMtMWZhNDI2YzE2MGVi.m3u8',
+    imagem: 'https://cdn.syncdev.com.br//uploads/video_thumb/12881.jpg',
+    ano: 2024,
+    usuarios: [10001]
+  },
+  {
+    id: '0005',
+    nome: 'Reality de Horror - Influencers em Pânico',
+    link: 'https://space.vscine.biz/filmes/AmpHouseMassacre/playlist.m3u8',
+    imagem: 'https://cdn.syncdev.com.br//uploads/video_thumb/12157.jpg',
+    ano: 2024,
+    usuarios: [10001]
+  }
+];
+
+/*
+{
+    id: '',
+    nome: '',
+    link: '',
+    imagem: '',
+    ano: ,
+    usuarios: [10001]
+  }
+*/
---
+++ script.js
@@ -0,0 +1,40 @@
+const catalogo = document.querySelector('ul');
+
+listaDeFilmes.forEach(filme => {
+  const li = document.createElement('li');
+
+  li.addEventListener('click', e => {
+    let idDaImagemClicada = e.target['id'];
+    console.log(idDaImagemClicada)
+    let usuario = Number(prompt('Senha:'));
+
+    listaDeFilmes.forEach(filme => {
+      if(filme.id === idDaImagemClicada){
+        if(filme.usuarios.includes(usuario)){
+          return
+        } else{
+          alert('Desculpe! Você não possui acesso a este filme.')
+          e.preventDefault()
+        } 
+      }
+    })
+    return
+  })
+
+  const img = document.createElement('img');
+  img.src = filme.imagem;
+  img.alt = filme.nome;
+  img.setAttribute('id', filme.id);
+
+  const p = document.createElement('p');
+  p.textContent = filme.ano;
+
+  const link = document.createElement('a');
+  link.href = `player.html?link=${encodeURIComponent(filme.link)}&nome=${encodeURIComponent(filme.nome)}`;
+  //link.target = '_blank';  // Abrir em nova aba
+  link.appendChild(img);
+
+  li.appendChild(link);
+  li.appendChild(p);
+  catalogo.appendChild(li);
+});
