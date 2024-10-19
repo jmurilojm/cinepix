51e117ffc96f7c59969bdd7580407984594c5d42
José Murilo <82318481+jmurilojm@users.noreply.github.com>
2024-10-19 16:09:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,19 @@ listaDeFilmes.forEach(filme => {
 
   li.addEventListener('click', e => {
     let idDaImagemClicada = e.target['id'];
-    console.log(idDaImagemClicada)
     let usuario = Number(prompt('Senha:'));
 
     listaDeFilmes.forEach(filme => {
       if(filme.id === idDaImagemClicada){
         if(filme.usuarios.includes(usuario)){
-          return
+          return;
         } else{
-          alert('Desculpe! Você não possui acesso a este filme.')
-          e.preventDefault()
+          e.preventDefault();
+          return;
         } 
       }
     })
-    return
+    return;
   })
 
   const img = document.createElement('img');
@@ -27,13 +26,20 @@ listaDeFilmes.forEach(filme => {
   img.setAttribute('id', filme.id);
 
   const p = document.createElement('p');
-  p.textContent = filme.ano;
+  p.innerHTML = filme.icone;
+  const p2 = document.createElement('p');
+  p2.textContent = filme.ano;
+  p2.setAttribute('class', 'ano');
 
   const link = document.createElement('a');
   link.href = `player.html?link=${encodeURIComponent(filme.link)}&nome=${encodeURIComponent(filme.nome)}`;
+  //link.target = '_blank';  // Abrir em nova aba
   link.appendChild(img);
 
   li.appendChild(link);
   li.appendChild(p);
+  li.appendChild(p2);
   catalogo.appendChild(li);
 });
+
+alert('Bem-vindo!');
